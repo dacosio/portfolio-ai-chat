cf63b37a4953bc66832514db822f5493d22a3a94
Don Apollo Cosio <dacosio@gmail.com>
2023-04-08 22:18:52 -0700

    testuser chat message

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
