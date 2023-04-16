c4b250f443c158efce965f28771057ac28df186c
Don Apollo Cosio <dacosio@gmail.com>
2023-04-15 20:47:53 -0700

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//   },
+//   resolve: {
+//     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }], //this will always lead to src wherever directory you are
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   build: {
-    outDir: "dist",
+    outDir: "build",
   },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }], //this will always lead to src wherever directory you are
