b88fa8598a3f83e27a30fd64f52993ea4b4e2453
Don Apollo Cosio <dacosio@gmail.com>
2023-04-15 20:24:15 -0700

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }], //this will always lead to src wherever directory you are
   },
